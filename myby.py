2c0c3426d0490d0237636dc60ba2a48938f62888
Nathan974-creator <113228775+Nathan974-creator@users.noreply.github.com>
2024-02-09 08:31:12 +0400

    add custom selector

---
+++ myby.py
@@ -0,0 +1,15 @@
+
+
+class by:
+    """
+    Set of supported locator strategies.
+    """
+
+    id = "id"
+    xpath = "xpath"
+    link_text = "link text"
+    partial_link_text = "partial link text"
+    name = "name"
+    tag_name = "tag name"
+    class_name = "class name"
+    css_selector = "css selector"
