c7959b42c7d73c56920c1676557d4dd817bac9a3
Nathan974-creator <113228775+Nathan974-creator@users.noreply.github.com>
2024-02-09 08:34:15 +0400

    Add init

--- roundcudeapi.py
+++ roundcudeapi.py
@@ -1,2 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options
+from myby import by
+import json
+
+
+class RoundcudeAPI:
+
+    def __init__(self, config_file: str = "config.json") -> None:
+        with open(config_file) as f:
+            config: dict = json.load(f)
+        
+        self.username = config.get("username")
+        self.password = config.get("password")
